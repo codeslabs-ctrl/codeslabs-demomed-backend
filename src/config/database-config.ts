2c1deeb07626684dd9d70898e46687eee7645223
Ramon Eduardo Gonzalez Benitez <Ramon.Gonzalez@farmatodo.com>
2026-03-13 15:41:14 -0400

    ajustes varios

--- src/config/database-config.ts
+++ src/config/database-config.ts
@@ -3,7 +3,7 @@
  * 
  * This system now uses PostgreSQL exclusively.
  * Supabase support has been removed.
- * Generated at: 2026-03-12T17:16:35.401Z
+ * Generated at: 2026-03-13T13:44:25.701Z
  */
 
 // Always use PostgreSQL
